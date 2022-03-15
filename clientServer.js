bf3e98ebc826f742fe7b72c97d622bd17241c4fb
Ddd Ddd <sjbsj@gmai.com>
2022-03-15 17:58:55 +0300

    Trying deploy app(

---
+++ clientServer.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
